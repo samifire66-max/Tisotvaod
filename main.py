bbc33b9a3443e5a51b9fd6a501356c34cd278188
samifire66-max <samifire66@gmail.com>
2026-07-19 20:11:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-import os
 from telegram import send_message
 
-APP_NAME = "טיסות ועוד"
-APP_VERSION = "0.1"
+send_message(
+"""🚀
 
+טיסות ועוד
 
-def main():
+גרסה 0.1 התחילה לעבוד.
 
-    print(f"{APP_NAME} {APP_VERSION}")
-
-    send_message(
-        "🚀 מנוע החיפוש הופעל בהצלחה.\n"
-        "המערכת מוכנה להתחיל בחיפוש עסקאות."
-    )
-
-    print("System started successfully.")
-
-
-if __name__ == "__main__":
-    main()
+אם קיבלת את ההודעה הזו,
+התשתית עובדת.
+"""
+) 
