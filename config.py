67481981cc4209ecfec59bab83168772be376d8e
samifire66-max <samifire66@gmail.com>
2026-07-19 20:07:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+MAX_PRICE = 4000
+
+ADULTS = 2
+CHILDREN = [14, 12, 9]
+
+DIRECT_ONLY = True
+
+AIRPORTS = [
+    "TLV",
+    "HFA"
+]
+
+TRIP_OPTIONS = [
+    (3, 4),   # חמישי-ראשון
+    (2, 3)    # שישי-ראשון
+]
