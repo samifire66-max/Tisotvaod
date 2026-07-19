98141247e4a670e76fb32f5418b00f530f7348df
samifire66-max <samifire66@gmail.com>
2026-07-19 20:10:13 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,30 +1,16 @@
 import requests
-
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "1238261691"
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
 
 def send_message(text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-    data = {
-        "chat_id": CHAT_ID,
-        "text": text,
-        "parse_mode": "HTML",
-        "disable_web_page_preview": False
-    }
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
-    response = requests.post(url, json=data)
-
-    if response.status_code == 200:
-        print("Telegram message sent.")
-    else:
-        print("Telegram error:")
-        print(response.text)
-
-
-if __name__ == "__main__":
-    send_message(
-        "✅ מערכת <b>טיסות ועוד</b> התחברה בהצלחה!\n\n"
-        "זוהי הודעת בדיקה ראשונה."
+    requests.post(
+        url,
+        json={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": text
+        },
+        timeout=30
     )
