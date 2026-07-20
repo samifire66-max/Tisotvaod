704448887b3bb11cd21d76ea965a6a7392c5dbea
samifire66-max <samifire66@gmail.com>
2026-07-20 21:25:50 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,46 @@
+# ==========================
+# Project Settings
+# ==========================
+
+SEARCH = {
+
+    # מאילו שדות תעופה מחפשים
+    "origins": [
+        "TLV",
+        "HFA"
+    ],
+
+    # לאן
+    "destinations": [
+        "ANY"
+    ],
+
+    # תקציב כולל
+    "max_price": 4000,
+
+    # נוסעים
+    "adults": 2,
+
+    "children_ages": [
+        14,
+        12,
+        9
+    ],
+
+    # משך החופשה
+    "trip_lengths": [
+        2,
+        3,
+        4
+    ],
+
+    # ימי יציאה מועדפים
+    "departure_days": [
+        "Thursday",
+        "Friday"
+    ],
+
+    # כמה עסקאות לשלוח בכל הרצה
+    "max_results": 10
+
+}
