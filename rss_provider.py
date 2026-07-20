bdecabc09ca440296879b7e1514eea1e04545fc5
samifire66-max <samifire66@gmail.com>
2026-07-20 15:43:49 +0300

    Update rss_provider.py

--- rss_provider.py
+++ rss_provider.py
@@ -1,1 +1,24 @@
 
+from normalizer import normalize
+
+
+def get_rss_deals():
+
+    sample = [
+        {
+            "title": "Weekend in Cyprus",
+            "destination": "Larnaca",
+            "price": 3290,
+            "link": "https://example.com/deal1",
+            "source": "RSS"
+        },
+        {
+            "title": "Weekend in Greece",
+            "destination": "Athens",
+            "price": 4890,
+            "link": "https://example.com/deal2",
+            "source": "RSS"
+        }
+    ]
+
+    return [normalize(x) for x in sample]
