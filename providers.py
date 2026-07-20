6c8faac830abb7f767f579d89deb8241bab2c4b2
samifire66-max <samifire66@gmail.com>
2026-07-20 21:46:27 +0300

    Update providers.py

--- providers.py
+++ providers.py
@@ -1,10 +1,17 @@
 from rss_provider import get_rss_deals
+from relevance import is_relevant
 
 
 def get_all_deals():
 
     deals = []
 
-    deals.extend(get_rss_deals())
+    rss = get_rss_deals()
+
+    for deal in rss:
+
+        if is_relevant(deal.title):
+
+            deals.append(deal)
 
     return deals
