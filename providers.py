5bd36a3f5f58e09a9bbdd2787507e82cdadccb42
samifire66-max <samifire66@gmail.com>
2026-07-20 21:19:30 +0300

    Update providers.py

--- providers.py
+++ providers.py
@@ -2,11 +2,9 @@ from rss_provider import get_rss_deals
 
 
 def get_all_deals():
+
     deals = []
 
-    try:
-        deals.extend(get_rss_deals())
-    except Exception as e:
-        print(f"RSS provider failed: {e}")
+    deals.extend(get_rss_deals())
 
     return deals
