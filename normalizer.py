8bb5665d4692e01035168af07ffaf907a22e498c
samifire66-max <samifire66@gmail.com>
2026-07-20 15:57:46 +0300

    Update normalizer.py

--- normalizer.py
+++ normalizer.py
@@ -1,10 +1,10 @@
-
 def normalize(deal):
 
     return {
         "title": deal.get("title", ""),
-        "price": int(deal.get("price", 999999)),
         "destination": deal.get("destination", ""),
+        "price": deal.get("price", 999999),
         "link": deal.get("link", ""),
-        "source": deal.get("source", "")
+        "source": deal.get("source", ""),
+        "published": deal.get("published", "")
     }
