937e45080efc41458781bb02404f6cdca0ed2b79
samifire66-max <samifire66@gmail.com>
2026-07-23 18:42:10 +0300

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,7 +1,27 @@
-RSS_FEEDS = [
-
-    "https://www.theflightdeal.com/feed/",
-
-    "https://travelfree.info/feed/"
-
+SOURCES = [
+    {
+        "name": "The Flight Deal",
+        "url": "https://www.theflightdeal.com/feed/",
+        "weight": 10,
+    },
+    {
+        "name": "TravelFree",
+        "url": "https://travelfree.info/feed/",
+        "weight": 9,
+    },
+    {
+        "name": "Travel-Dealz",
+        "url": "https://travel-dealz.com/feed/",
+        "weight": 9,
+    },
+    {
+        "name": "I Know The Pilot",
+        "url": "https://iknowthepilot.com.au/feed/",
+        "weight": 8,
+    },
+    {
+        "name": "Fare Deal Alert",
+        "url": "https://faredealalert.com/feed/",
+        "weight": 7,
+    },
 ]
