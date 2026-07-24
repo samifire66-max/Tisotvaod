5ab89de47c6e335e430305bbb3029f41ef2edb78
samifire66-max <samifire66@gmail.com>
2026-07-24 17:44:17 +0300

    Update providers.py

--- providers.py
+++ providers.py
@@ -2,18 +2,19 @@ from rss_provider import get_rss_deals
 from relevance import is_relevant
 
 
-from rss_provider import get_rss_deals
-from relevance import is_relevant
-
-
 def get_all_deals():
 
-    deals = []
+    all_deals = get_rss_deals()
 
-    for deal in get_rss_deals():
+    print(f"Collected from RSS: {len(all_deals)}")
+
+    relevant = []
+
+    for deal in all_deals:
 
         if is_relevant(deal):
+            relevant.append(deal)
 
-            deals.append(deal)
+    print(f"Relevant deals: {len(relevant)}")
 
-    return deals
+    return relevant
